e966be1e9742b252338093566cee74a1af35a6a7
Nuri Jung <jnooree@snu.ac.kr>
2024-12-06 15:35:07 +0900

    ci: update build config

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+output="libabsl-static-${1}.tar.gz"
+set --
+
+cmake -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_EXTENSIONS=OFF \
+	-DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+	-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DCMAKE_POLICY_DEFAULT_CMP0069=NEW \
+	-DCMAKE_INSTALL_PREFIX=./install -DABSL_BUILD_TESTING=ON \
+	-S abseil-cpp -B build -G Ninja
+
+cmake --build build -j --target all
+ctest -T build --output-on-failure -j
+cmake --install build
+
+tar -cvzf "${output}" -C install .
