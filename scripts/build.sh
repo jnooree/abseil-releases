34b0738785c300dbae3f22032c60cca0f42cd7b9
Nuri Jung <jnooree@snu.ac.kr>
2024-12-06 16:20:06 +0900

    feat: try building universal binary

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,11 +9,18 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./install \
 	-DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_EXTENSIONS=OFF \
 	-DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
 	-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DCMAKE_POLICY_DEFAULT_CMP0069=NEW \
+	'-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64' -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 \
 	-DBUILD_TESTING=ON -DABSL_BUILD_TESTING=ON -DABSL_USE_GOOGLETEST_HEAD=ON \
 	-S abseil-cpp -B build -G Ninja
 
 cmake --build build -j --target all
-ctest --test-dir build -T test --output-on-failure -j
-cmake --install build
 
+if [[ $output == *-macosx_* ]]; then
+	arch -arm64 ctest --test-dir build -T test --output-on-failure -j
+	arch -x86_64 ctest --test-dir build -T test --output-on-failure -j
+else
+	ctest --test-dir build -T test --output-on-failure -j
+fi
+
+cmake --install build
 tar -cvzf "${output}" -C install .
