0f1857c4f8c3589587185713c3b5713705f9eae4
Nuri Jung <jnooree@snu.ac.kr>
2025-04-17 12:18:14 +0900

    feat: move installation command into build file and show compilation cmd

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 output="libabsl-static-${1}.tar.gz"
 set --
 
+if [[ $output == *linux* ]]; then
+	yum -y install ninja-build
+fi
+
 cmake_configure_args_common=(
 	-DCMAKE_BUILD_TYPE=Release
 	-DCMAKE_INSTALL_PREFIX=./install
@@ -13,6 +17,7 @@ cmake_configure_args_common=(
 	-DCMAKE_CXX_STANDARD_REQUIRED=ON
 	-DCMAKE_POSITION_INDEPENDENT_CODE=ON
 	'-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64'
+	-DCMAKE_VERBOSE_MAKEFILE=ON
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.13
 	-DABSL_PROPAGATE_CXX_STD=ON
 	-Sabseil-cpp
